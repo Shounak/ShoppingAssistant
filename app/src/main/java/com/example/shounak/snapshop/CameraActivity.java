6da3413cb47a0fd42e33f71e5a5fd1394add0af9
Shounak Kulkarni <shounakkul@gmail.com>
2016-11-12 23:18:12 -0600

    Added camera activity

--- app/src/main/java/com/example/shounak/snapshop/CameraActivity.java
+++ app/src/main/java/com/example/shounak/snapshop/CameraActivity.java
@@ -1,7 +1,11 @@
 package com.example.shounak.snapshop;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        Camera camera = null;
+        if (checkCameraHardware(this)) {
+            try {
+                camera = Camera.open(); // attempt to get a Camera instance
+            }
+            catch (Exception e){
+                // Camera is not available (in use or does not exist)
+            }
+        }
+        if (camera != null) {
+
+        }
+        else {
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+            int i = 0;
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
+
+
